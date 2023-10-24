d38dfefd52ce00734febce722749c72ce9a10011
BryanHernz <126428695+BryanHernz@users.noreply.github.com>
2023-10-24 17:57:01 -0300

    fix_server_version 3

--- models/server.ts
+++ models/server.ts
@@ -81,7 +81,12 @@ class Server {
             allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
             credentials: true
         }));
-
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            next();
+          });
         //Lectura del body
         this.app.use( json() );
 
