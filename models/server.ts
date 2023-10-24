d2391030dc2cbce3f890fe7a29a92d9488d9b74d
BryanHernz <126428695+BryanHernz@users.noreply.github.com>
2023-10-24 19:47:31 -0300

    fix_server_version 7

--- models/server.ts
+++ models/server.ts
@@ -75,10 +75,12 @@ class Server {
     middlewares(){
 
         //CORS
-        this.app.use( cors({
-            origin: '*',
-            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-          }));
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*'); // Reemplaza con tu dominio local
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            next();
+          });
         //Lectura del body
         this.app.use( json() );
 
