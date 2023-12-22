a85901d24a8311d92ed957d7e7ceabb4d4ef7e06
BryanHernz <126428695+BryanHernz@users.noreply.github.com>
2023-12-22 01:09:08 -0300

    Update migration.sql

--- prisma/migrations/20231219234633_/migration.sql
+++ prisma/migrations/20231219234633_/migration.sql
@@ -14,4 +14,4 @@ ALTER TABLE `products` MODIFY `length` DECIMAL NULL,
     MODIFY `height` DECIMAL NULL;
 
 -- AddForeignKey
-ALTER TABLE `notifications` ADD CONSTRAINT `notifications_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `notifications` ADD CONSTRAINT `notifications_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
