75fc59c59dff46f1b66eca037715e71851ee4639
BryanHernz <126428695+BryanHernz@users.noreply.github.com>
2024-01-03 11:14:15 -0300

    final fixes

--- prisma/migrations/20231005181346_creating_provinces_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `provinces` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(225) NOT NULL,
-
-    UNIQUE INDEX `id`(`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
